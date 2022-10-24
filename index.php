469ed2c1d68948b9a61383a3ba98d5e86b35f1d4
Simon Balleux <simoncestmoi@hotmail.fr>
2022-10-24 14:34:04 +0200

    index.php OK, logique applications ok et integration de mon autoload dans mon app

--- index.php
+++ index.php
@@ -9,3 +9,34 @@ error_reporting(E_ALL);
 define('ROOT', dirname(__DIR__.'/MVC_PHP_P5_V1'));
 
 // echo ROOT;
+
+
+// Inclure mon autoloader
+Require_once ROOT.'/App/Autoloader.php';
+
+// spl_autoload dans mon Autoloader.php()
+App\Autoloader::spl_autoload();
+
+
+// Si @param page n'est pas definis, par default il sera a home
+$page = 'home';
+
+// Appéler la methode voulu, definit dans le HomeController
+$method = 'DataBdd';
+
+
+// Si @param page est definis, alors sa valeur sera la valeur du param url
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+}
+
+if (isset($_GET['method'])) {
+    $method = $_GET['method'];
+}
+
+// Definir dynamiquement mon chemin pour le controlleur voulu
+$nameController = 'App\Controller\\'.ucfirst($page).'Controller';
+$controller = new $nameController;
+
+// Appel de ma method DataBdd
+$controller->$method();
