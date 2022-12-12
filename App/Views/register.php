c4216a93afa800d9b4059b8f87ee54a047bca458
Simon Balleux <simoncestmoi@hotmail.fr>
2022-12-12 11:56:05 +0100

    Merge de ma branch EditPost

--- App/Views/register.php
+++ App/Views/register.php
@@ -12,7 +12,7 @@
             <input type="text" class="box-input" name="email" placeholder="Email" required />
             <input type="password" class="box-input" name="password" placeholder="Mot de passe" required />
             <input type="submit" name="submit" value="S'inscrire" class="box-button" />
-            <p class="box-register">Déjà inscrit? <a href="?page=login">Connectez-vous ici</a></p>
+            <p class="box-register">Déjà inscrit ? <a href="?page=login">Connectez-vous ici</a></p>
         </form>
     </body>
 </html>
